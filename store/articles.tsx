b3de1e20c007890e7d1f7cd4abb896616baa0ec5
Mark Toh <marktoh96@gmail.com>
2024-02-12 21:46:39 +0800

    feat: update mock data

--- store/articles.tsx
+++ store/articles.tsx
@@ -3,13 +3,27 @@ import GibsonLesPaul from "../public/assets/articles/gibson-lespaul.jpg";
 import AcousticGuitar from "../public/assets/articles/acoustic-guitar.jpg";
 import GuitarPlayer from "../public/assets/articles/guitar-player.jpg";
 
-const images = [GuitarPedals, GibsonLesPaul, AcousticGuitar, GuitarPlayer];
-
-const baseData = {
-  title: "Article Title",
-  description: "Article Subhead",
-};
-
-const articles = images.map((image) => ({ ...baseData, image }));
+const articles = [
+  {
+    image: GuitarPedals,
+    title: "Guitar Pedals 101",
+    description: "Introduction to pedals",
+  },
+  {
+    image: GibsonLesPaul,
+    title: "Guitar Reborn",
+    description: "The Goldtop Reissue is here",
+  },
+  {
+    image: AcousticGuitar,
+    title: "Acoustics Again",
+    description: "Dynamics with a pro",
+  },
+  {
+    image: GuitarPlayer,
+    title: "Electric Guitars",
+    description: "Engage with a professional",
+  },
+];
 
 export { articles };
--- store/products.tsx
+++ store/products.tsx
@@ -1,30 +1,114 @@
 import GibsonLesPaul from "../public/assets/new-products/gibson-lespaul.png";
 import EpiphoneSG from "../public/assets/new-products/epiphone-sg.png";
 import FenderStrat from "../public/assets/new-products/fender-strat.png";
-import FenderJaguar from "../public/assets/new-products/fender-jaguar.png";
+import FenderJazzmaster from "../public/assets/new-products/fender-jazzmaster.png";
 
 import GreenCable from "../public/assets/popular-finds/green-cable.png";
 import YellowCable from "../public/assets/popular-finds/yellow-cable.png";
 import Polytune from "../public/assets/popular-finds/polytune.png";
 import ErnieBall from "../public/assets/popular-finds/ernieball.png";
 
-const images = {
-  new: [GibsonLesPaul, EpiphoneSG, FenderStrat, FenderJaguar],
-  popular: [GreenCable, YellowCable, Polytune, ErnieBall],
-};
+const products = [
+  {
+    image: GibsonLesPaul,
+    title: "Gibson Les Paul",
+    description: "'56 Goldtop Reissue",
+    price: "4,999.99",
+    chips: [
+      {
+        count: 2,
+        type: "colors",
+      },
+    ],
+  },
+  {
+    image: EpiphoneSG,
+    title: "Epiphone SG",
+    description: "Legendary 1960s Classic",
+    price: "449.99",
+    chips: [
+      {
+        count: 3,
+        type: "colors",
+      },
+    ],
+  },
+  {
+    image: FenderStrat,
+    title: "Fender Strat",
+    description: "Dual-tone Sunburst",
+    price: "799.99",
+    chips: [
+      {
+        count: 2,
+        type: "colors",
+      },
+    ],
+  },
+  {
+    image: FenderJazzmaster,
+    title: "Jazzmaster",
+    description: "Solid Orange",
+    price: "829.99",
+    chips: [
+      {
+        count: 3,
+        type: "colors",
+      },
+    ],
+  },
+];
 
-const baseData = {
-  title: "Brand Name",
-  description: "Product description here",
-  price: "2,995",
-  chips: [
-    {
-      count: 0,
-      type: "colors",
-    },
-  ],
-};
+const popularProducts = [
+  {
+    image: GreenCable,
+    title: "Fender Cable",
+    description: "Original Series",
+    price: "27.99",
+    chips: [
+      {
+        count: 3,
+        type: "colors",
+      },
+    ],
+  },
+  {
+    image: YellowCable,
+    title: "Fender Cable",
+    description: "Deluxe Series",
+    price: "37.99",
+    chips: [
+      {
+        count: 3,
+        type: "colors",
+      },
+    ],
+  },
+  {
+    image: Polytune,
+    title: "TC Polytune 3",
+    description: "Modern Tuner",
+    price: "79.99",
+    chips: [
+      {
+        count: 2,
+        type: "colors",
+      },
+    ],
+  },
+  {
+    image: ErnieBall,
+    title: "Ernie Ball Skinny",
+    description: "Nickel Wound",
+    price: "11.99",
+    chips: [
+      {
+        count: 3,
+        type: "colors",
+      },
+    ],
+  },
+];
 
-const newProducts = images.new.map((image) => ({ ...baseData, image }));
-const popularProducts = images.popular.map((image) => ({ ...baseData, image }));
+const newProducts = products;
 export { newProducts, popularProducts };
